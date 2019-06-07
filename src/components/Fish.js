4e61d0c44e72b50190e70bdcc622a67d34df002f
thanhphu95 <thanhphu6595@gmail.com>
2019-06-07 14:34:14 +0700

    Add not found component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 class Fish extends Component {
     render() {
         return (
-            <Col sm={12}>
-                Here is Fish
-            </Col>
+            <Container>
+                <Col sm={12}>
+                    Here is Fish
+                </Col>
+            </Container>
         )
     }
 }
---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+class NotFound extends Component {
+    render() {
+        return(
+            <Container>
+                <h3> 404 Not Found</h3>
+            </Container>
+        );
+    }
+}
+
+export default NotFound;
